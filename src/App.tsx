d8f259b5f192f50ce0157093e18cef5e76183771
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-08-25 19:45:58 +0300

    feat: card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Sidebar from "./components/Sidebar/Sidebar.tsx";
+import MainDash from "./components/MainDash/MainDash.tsx";
 
 function App() {
   return (
     <div className="App">
         <div className="AppGlass">
             <Sidebar/>
+            <MainDash/>
         </div>
     </div>
   );
---
+++ src/components/MainDash/MainDash.tsx
@@ -0,0 +1,13 @@
+import './MainDash.css';
+import Cards from "./Cards/Cards.tsx";
+
+const MainDash = () => {
+    return (
+        <div className="MainDash">
+            <h1>Dashboard</h1>
+            <Cards/>
+        </div>
+    );
+}
+
+export default MainDash;
